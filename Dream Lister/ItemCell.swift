64e8e6c19c7a7905518e53c14725a3f20a9578b5
Melissa Bain <apps@melissadbain.com>
2016-08-29 11:49:44 -0500

    Connecting the views completed

---
+++ Dream Lister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  Dream Lister
+//
+//  Created by Melissa Bain on 8/29/16.
+//  Copyright © 2016 MB Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
